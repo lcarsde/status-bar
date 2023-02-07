4f5cdba680c57c133379f0ed00c465810f28efac
Andreas Tennert <mail@andreas-tennert.de>
2023-02-07 03:38:42 +0100

    refactor: make CPU utilization code more readable

--- src/nativeMain/kotlin/de/atennert/lcarsde/statusbar/widgets/CpuUsageWidget.kt
+++ src/nativeMain/kotlin/de/atennert/lcarsde/statusbar/widgets/CpuUsageWidget.kt
@@ -8,10 +8,14 @@ import statusbar.GtkCssProvider
 class CpuUsageWidget(widgetConfiguration: WidgetConfiguration, cssProvider: CPointer<GtkCssProvider>) :
     RadarGraphWidget(widgetConfiguration, cssProvider, 500, 100) {
 
+    data class CpuUse(val idle: Float, val total: Float) {
+        operator fun minus(cpuUse2: CpuUse) = CpuUse(cpuUse2.idle - this.idle, cpuUse2.total - this.total)
+    }
+
     override val attentionValue = 60.0
     override val warningValue = 80.0
 
-    private var lastIdlesTotals: List<Pair<Float, Float>> = emptyList()
+    private var lastCpuUses: List<CpuUse> = emptyList()
 
     override fun getData(): List<Double> = getCpuUtilization()
 
@@ -26,16 +30,16 @@ class CpuUsageWidget(widgetConfiguration: WidgetConfiguration, cssProvider: CPoi
                     .drop(1)
                     .map(String::toFloat)
             }
-        val idlesTotals = cpuData.map { Pair(it[3] + it[4], it.sum()) }
+        val cpuUses = cpuData.map { CpuUse(it[3] + it[4], it.sum()) }
 
-        val deltas = if (lastIdlesTotals.isEmpty()) {
-            Array(idlesTotals.size) { Pair(1f, 1f) }.toList()
+        val cpuUseDelta = if (lastCpuUses.isEmpty()) {
+            List(cpuUses.size) { CpuUse(1f, 1f) }
         } else {
-            idlesTotals.zip(lastIdlesTotals) { (c1, c2), (l1, l2) -> Pair(c1 - l1, c2 - l2) }
+            cpuUses.zip(lastCpuUses) { currentCpuUse, lastCpuUse -> currentCpuUse - lastCpuUse }
         }
-        lastIdlesTotals = idlesTotals
+        lastCpuUses = cpuUses
 
-        return deltas.map { (idle, total) ->
+        return cpuUseDelta.map { (idle, total) ->
             try {
                 100 * (1.0 - idle / total)
             } catch (e: ArithmeticException) {
